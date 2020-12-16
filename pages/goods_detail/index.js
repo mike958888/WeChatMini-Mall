4930dddc0a63b1a128e435b8cee3c002a82f10ac
mike958888 <mike958888@gmail.com>
2020-12-16 15:07:41 +0800

    优化渲染数据

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,23 @@
-// pages/goods_detail/index.js
+import {request} from '../../request/index'
+import regeneratorRuntime from '../../lib/runtime/runtime'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsDetail:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    const {goods_id} = options;
+    this.getGoodsDetail(goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async getGoodsDetail(goods_id){
+    const goodsDetail = await request({url:'/goods/detail',data:{goods_id}})
+    this.setData({
+      goodsDetail:{
+        goods_name:goodsDetail.goods_name,
+        goods_price:goodsDetail.goods_price,
+        pics:goodsDetail.pics,
+        goods_introduce:goodsDetail.goods_introduce.replace(/\.wep/g,'.jpg')
+      }
+    })
   }
 })
--- request/index.js
+++ request/index.js
@@ -1,4 +1,10 @@
+let ajaxTimes = 0
 export const request=(params)=>{
+    ajaxTimes ++
+    wx.showLoading({
+        title: '加载中',
+        mask:true
+      })
     // 定义公共的接口
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1"
     return new Promise((resolve,reject)=>{
@@ -11,6 +17,12 @@ export const request=(params)=>{
             fail: (err) =>{
                 reject(err)
             },
+            complete: function() {
+                ajaxTimes--;
+                if(ajaxTimes===0){
+                    wx.hideLoading()
+                }
+            }
         })
     })
 }
