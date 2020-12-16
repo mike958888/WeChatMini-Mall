0607c21e6e221daa695390d90a580825940c0932
mike958888 <mike958888@gmail.com>
2020-12-16 22:18:25 +0800

    商品详情页完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -5,12 +5,15 @@ Page({
   data: {
     goodsDetail:{}
   },
+  GoodsInfo:[],
   onLoad: function (options) {
     const {goods_id} = options;
     this.getGoodsDetail(goods_id)
   },
   async getGoodsDetail(goods_id){
     const goodsDetail = await request({url:'/goods/detail',data:{goods_id}})
+    this.GoodsInfo = goodsDetail
+    // console.log(this.GoodsInfo);
     this.setData({
       goodsDetail:{
         goods_name:goodsDetail.goods_name,
@@ -18,6 +21,39 @@ Page({
         pics:goodsDetail.pics,
         goods_introduce:goodsDetail.goods_introduce.replace(/\.wep/g,'.jpg')
       }
+    })
+  },
+  imagePreview(e){
+    const urls = this.GoodsInfo.pics.map(v=>v.pics_mid)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current,
+      urls
+    })
+  },
+  handleGoodsAdd(){
+    // 获取缓存中的购物车  数组
+    let cart = wx.getStorageSync('cart') || [];
+    // 判断购物车是否存在该商品
+    let index = cart.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id)
+    // console.log(index);
+    // 若不存在，第一次添加
+    if(index=== -1){
+      this.GoodsInfo.num =1;
+      cart.push(this.GoodsInfo)
+    }else{
+       // 若存在，数据加
+       cart[index].num++
+       console.log(cart);
+    }
+    // 把购物车数据从新添加到缓存
+    wx.setStorageSync('cart', cart)
+    // 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      // 防抖
+      mask:true,
     })
   }
 })
